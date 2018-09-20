c8b7a2737b8306bc2d5b4053d45e6f6162547939
zhangyu1992 <zy334451241@sina.com>
2018-09-20 10:23:42 +0800

    Initial Commit

---
+++ SDKFrameWork/SDKFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  SDKFrameWork.h
+//  SDKFrameWork
+//
+//  Created by 张张 on 2018/9/20.
+//  Copyright © 2018年 zhangzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKFrameWork.
+FOUNDATION_EXPORT double SDKFrameWorkVersionNumber;
+
+//! Project version string for SDKFrameWork.
+FOUNDATION_EXPORT const unsigned char SDKFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKFrameWork/PublicHeader.h>
+
+
---
+++ SDKFrameWorkTests/SDKFrameWorkTests.m
@@ -0,0 +1,39 @@
+//
+//  SDKFrameWorkTests.m
+//  SDKFrameWorkTests
+//
+//  Created by 张张 on 2018/9/20.
+//  Copyright © 2018年 zhangzhang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SDKFrameWorkTests : XCTestCase
+
+@end
+
+@implementation SDKFrameWorkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
